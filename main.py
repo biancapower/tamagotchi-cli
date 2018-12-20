12cd51731c8579367e99ad1a0ece51ebe1d2a479
Bianca Power <30207144+biancapower@users.noreply.github.com>
2018-12-21 09:21:48 +1100

    loop over input

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ hunger = 5
 health = 5
 happiness = 5
 
-print(f'____________________\nName: {name}\nAge: {age}\nHunger: {hunger}\nHealth: {health}\nHappiness: {happiness}\n____________________')
-
-command = input('Press (f) to feed, (e) to exercise, or (p) to play ')
-
-if command == 'f':
-    hunger = hunger + 1
-elif command == 'e':
-    health = health + 1
-elif command == 'p':
-    happiness = happiness + 1
-    
+running = True
 
 print(f'____________________\nName: {name}\nAge: {age}\nHunger: {hunger}\nHealth: {health}\nHappiness: {happiness}\n____________________')
+
+while running:
+
+    command = input('Press (f) to feed, (e) to exercise, or (p) to play ')
+
+    if command == 'f':
+        hunger = hunger + 1
+    elif command == 'e':
+        health = health + 1
+    elif command == 'p':
+        happiness = happiness + 1
+    else: # if invalid key pressed, decrease happiness
+        happiness = happiness - 1
+        
+
+    print(f'____________________\nName: {name}\nAge: {age}\nHunger: {hunger}\nHealth: {health}\nHappiness: {happiness}\n____________________')
