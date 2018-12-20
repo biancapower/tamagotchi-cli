c841d39e7f8fb01ceacfa91c1d1e57e940ebd8db
Bianca Power <30207144+biancapower@users.noreply.github.com>
2018-12-21 09:13:15 +1100

    change stats

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ happiness = 5
 print(f'____________________\nName: {name}\nAge: {age}\nHunger: {hunger}\nHealth: {health}\nHappiness: {happiness}\n____________________')
 
 command = input('Press (f) to feed, (e) to exercise, or (p) to play ')
+
+if command == 'f':
+    hunger = hunger + 1
+elif command == 'e':
+    health = health + 1
+elif command == 'p':
+    happiness = happiness + 1
+    
+
+print(f'____________________\nName: {name}\nAge: {age}\nHunger: {hunger}\nHealth: {health}\nHappiness: {happiness}\n____________________')
